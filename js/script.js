b6a52c011df7c66bcad9b0b5e8841b58e9e448af
trapdracula <mattlancaster2012@gmail.com>
2025-04-07 21:55:01 -0400

    added ifelse statements for points updating

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ const players = [
    { name: "Player 2", score: 0 },
    { name: "Player 3", score: 0 }
 ];
+const pts1 = rollDie1();
+const pts2 = rollDie2();
+const equityPile = 0;
+const ePts = rolls % 2;
+const rolls = pts1 + pts2;
 
 var diceContainer = document.getElementById("dice-container");
 var die1 = document.getElementById("die1")
@@ -21,19 +26,18 @@ function rollDie2() {
    return Math.floor(Math.random() * 6) + 1;
 }
 function diceRoller() {
-   var pts1 = rollDie1();
-   var pts2 = rollDie2();
    diceContainer.style.color = "Black";
    diceContainer.style.fontSize = "36px";
    die1.innerHTML = pts1;
    die2.innerHTML = pts2;
 }
-
 function updateScores() {
+   if (ePts < 0) {
+      equityPile += ePts
+   }
+   else {
+      players[0].score += (ePts / 2);
 
-}
-if (pts1 + pts2 % < 0) {
-
-} else {
+   }
 
 }
